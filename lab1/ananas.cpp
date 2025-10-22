d092a4d2a454ecb856877d0ab37a65d68c58fa6a
the0j0se <mixailguba2007@gmail.com>
2025-10-22 21:37:12 +0300

    Add files via upload

---
+++ lab1/ananas.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main()
+{
+    float x, y;
+    std::cout << "x: ";
+    std::cin >> x;
+    std::cout << "y: ";
+    std::cin >> y;
+    const double PI = 3.14159265358979323846;
+    double R = cos(PI/2 * x);
+    double S = log2(pow(2, y));
+    std::cout << "R = " << R << std::endl;
+    std::cout << "S = " << S << std::endl;
+    double C = std::max(R, S);
+    std::cout << "C = " << C << std::endl;
+
+    system("pause");
+    
+    return 0;
+}
