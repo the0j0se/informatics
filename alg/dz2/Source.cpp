fef302685c56d082ccb04c37dbe91d5bfd7fcc5f
the0j0se <mixailguba2007@gmail.com>
2026-05-25 00:07:17 +0300

    Add files via upload

---
+++ alg/dz2/Source.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <iomanip>
+#include "Header.h"
+
+int main() {
+    double answer = 1.0;
+    double a = 3.0;
+    double b = 5.0;
+    int arr[] = { 2, 3, 4, 5, 6, 8, 10, 50, 80, 100, 150, 200, 500, 1000 };
+
+    std::cout << "Priblizhenoe znachenie ploshadi pri" << std::endl;
+    for (int n : arr) {
+        double result = simpson_rule(integral, a, b, n);
+        std::cout << "n = " << n << " -> " << result << " pogreshonst: " 
+            << std::fixed << std::setprecision(6) << result - answer << std::endl;
+    }
+    std::cout << "Pravilnoe znachenie: " << answer << std::endl;
+
+    return 0;
+}
---
+++ alg/dz2/aaaaaaaaaa.cpp
@@ -0,0 +1,24 @@
+#include "Header.h"
+#include <cmath>
+
+double simpson_rule(double (*f)(double), double a, double b, int n) {
+    if (n % 2 == 1) n++;
+    double h = (b - a) / n;
+    double sum = f(a) + f(b);
+    for (int i = 1; i < n; i += 2)
+        sum += 4 * f(a + i * h);
+    for (int i = 2; i < n - 1; i += 2)
+        sum += 2 * f(a + i * h);
+    return (h / 3) * sum;
+}
+
+double integral(double x) {
+    double minimum = 3.0;
+    return upper_bound(x) - minimum;
+}
+
+
+double upper_bound(double x) {
+    if (x <= 4) return x;
+    return -x + 8;
+}
